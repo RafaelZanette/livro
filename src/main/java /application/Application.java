17cd3920e62f09924e0d9d847908fb887395a1f4
RafaelZanette <r.zanette91@gmail.com>
2021-08-24 19:56:26 +0000

    criação dos arquivos

--- src/main/java /application/Application.java
+++ src/main/java /application/Application.java
@@ -1,7 +1,20 @@
 package application; 
+import org.springframework.boot.SpringApplication;
+import org.springframework.book.autoconfigure.SpringBootApplication;
 
 public class application{
     public static void main  (String args[])
+
+        SpringApplication.run(Application.class, args);
+
+   
+    
+
+
+
+
+    }
+
 
 
 
---
+++ src/main/webapp/home/Index.jsp
@@ -0,0 +1,14 @@
+%@page language ="java" contentype = "text/html"; charset=UTF-8"
+pageEnconding="UTF-8"%;
+
+<!DOCYTPE html>
+
+<html>  
+    <head>
+        <meta charset="utf-8"/>
+        <title> Olá Mundo</title>
+    </head>
+            <body>
+                <h1>Olá Mundo Rafael</h1>
+            </body>
+</html> 
